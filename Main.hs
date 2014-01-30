7e66fadd530af5e6798cba357e6807191dd4f4d0
YoEight <yo.eight@gmail.com>
2014-01-30 19:36:43 +0100

    Add source header

--- Main.hs
+++ Main.hs
@@ -1,2 +1,14 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (C) 2013 Yorick Laupa
+-- License     :  (see the file LICENSE)
+--
+-- Maintainer  :  Yorick Laupa <yo.eight@gmail.com>
+-- Stability   :  provisional
+-- Portability :  non-portable
+--
+----------------------------------------------------------------------------
+
 main :: IO ()
 main = print "Hello Word"
